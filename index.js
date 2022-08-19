b8c513a1a3dddf921f6830f9124597cf1526029f
danielgathemia <danielgathemia01@gmail.com>
2022-08-19 18:54:41 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'mike'
+let welcomeMessage = "Welcome to Flatbook, mike!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, MIKE!"
+let shortGreeting = 'Welcome, m!'
